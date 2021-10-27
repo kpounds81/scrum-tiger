bc6a8c64e6fa4874c59f5dc940a5395926a52463
Alex Thaler <athaler@clearcover.com>
2021-10-27 15:50:33 -0500

    homepage content complete

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,11 +3,24 @@ import React from 'react'
 export const Home = () => {
   return(
     <div>
-      <h1>All About Tigers</h1>
+      <h1>Tons of fun. Lots of learning.</h1>
+      <br/>
+      <h1>All in one place.</h1>
       <p>
         <img src="/images/t1.jpeg" alt="tiger relaxing after dinner" />
         <div style={{paddingTop: '10px'}}>
-          Here you can see a beautiful tiger relaxing after a nice fillet of antelope.
+          Hey kids want to learn more about one of the most amazing predators on the planet.
+        </div>
+        <br/>
+        <div>
+          Its called the tiger it has stripes and big fangs!
+        </div>
+        <div>
+          <br/>
+          But if you were to talk to an expert at the zoo they would refer to them by their scientific name:
+          <ul>
+            <li>Panthera tigris</li>
+          </ul>
         </div>
       </p>
     </div>
