985c0f40018a12528367ea7e187117048304d673
Keith Pounds <kpounds@clearcover.com>
2021-10-28 14:28:30 -0500

    top nav changes

--- src/Components/TopNav.tsx
+++ src/Components/TopNav.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
 const TopNav = () => {
+  const [active, setActive] = useState('')
+  const {location} = useHistory()
+
+  useEffect(() => {
+    if (active === '') {
+      setActive(location.pathname)
+    }
+  }, [active, location.pathname])
+
   return (
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/classification">Famous Tigers</Link>
-        <Link to="/diet">Diet</Link>
-        <Link to="/images">Images</Link>
-        <Link to="/habitat">Habitats</Link>
+      <div className="nav">
+        <Link className={`link ${active === '/' ? 'active' : ''}`} onClick={() => setActive('/')} to="/">
+          Home
+        </Link>
+        <Link className={`link ${active === '/classification' ? 'active' : ''}`} onClick={() => setActive('/classification')} to="/classification">
+          Famous Tigers
+        </Link>
+        <Link className={`link ${active === '/diet' ? 'active' : ''}`} onClick={() => setActive('/diet')} to="/diet">
+          Diet
+        </Link>
+        <Link className={`link ${active === '/images' ? 'active' : ''}`} onClick={() => setActive('/images')} to="/images">
+          Images
+        </Link>
+        <Link className={`link ${active === '/habitat' ? 'active' : ''}`} onClick={() => setActive('/habitat')} to="/habitat">
+          Habitats
+        </Link>
         {/* <Link to="/anatomy">Anatomy</Link> */}
         {/* <Link to="/behavior">Behavior</Link> */}
         {/* <Link to="/mating">Mating</Link> */}
