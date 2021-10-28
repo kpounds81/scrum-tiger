857dce6e0121082317fb258eb069ad8f3d76e75e
Alex Thaler <athaler@clearcover.com>
2021-10-28 11:26:27 -0500

    adding diet page

--- src/Pages/Diet.tsx
+++ src/Pages/Diet.tsx
@@ -3,7 +3,23 @@ import React from 'react'
 export const Diet = () => {
   return(
     <div>
-      <h1>Diet Information</h1>
+      <h1>What do Tigers Eat?</h1>
+      <p>
+        <img src="/images/eating.png" alt="tiger" />
+      </p>
+      <h2> Tigers eat only meat and their favorite things to eat are:</h2>
+      <ul>
+        <li>
+          <h3>Kids</h3>
+          <h3>Deer</h3>
+          <h3>Antelope</h3>
+          <h3>Wild Boar</h3>
+          <h3>Buffalo</h3>
+          <h3>Monkeys</h3>
+          <h3>Porcupines</h3>
+          <h3>Rats</h3>
+        </li>
+      </ul>
     </div>
   )
 }
